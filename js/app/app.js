1b1f333049dc19d8d73047fc95b2a14b7c43f60c
Kwabena Adu-Darkwa <kwabenaadudarkwa@gmail.com>
2017-10-20 18:42:36 +0200

    edits

--- js/app/app.js
+++ js/app/app.js
@@ -87,15 +87,15 @@ function onMapError(error) {
         }
     }
 
-function detectBrowser() {
-  var useragent = navigator.userAgent;
-  var mapdiv = document.getElementById("map");
+// function detectBrowser() {
+//   var useragent = navigator.userAgent;
+//   var mapdiv = document.getElementById("map");
 
-  if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
-    mapdiv.style.width = '100%';
-    mapdiv.style.height = '100%';
-  } else {
-    mapdiv.style.width = '600px';
-    mapdiv.style.height = '800px';
-  }
-}
+//   if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+//     mapdiv.style.width = '100%';
+//     mapdiv.style.height = '100%';
+//   } else {
+//     mapdiv.style.width = '600px';
+//     mapdiv.style.height = '800px';
+//   }
+// }
